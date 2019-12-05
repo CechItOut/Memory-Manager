c42beaa0ae1da0d037a9fa35cd405cc59e65ef9a
Daniel R <55966399+CechItOut@users.noreply.github.com>
2019-12-05 12:23:43 -0500

    Add files via upload

---
+++ Memory Manager/Form1.Designer.vb
@@ -0,0 +1,204 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.addProcess = New System.Windows.Forms.Button()
+        Me.processSize = New System.Windows.Forms.TextBox()
+        Me.selectedProcess = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.removeProcess = New System.Windows.Forms.Button()
+        Me.selectedAlgorithm = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.compactMemory = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.maxSize = New System.Windows.Forms.TextBox()
+        Me.changeSize = New System.Windows.Forms.Button()
+        Me.exitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'addProcess
+        '
+        Me.addProcess.Location = New System.Drawing.Point(76, 256)
+        Me.addProcess.Name = "addProcess"
+        Me.addProcess.Size = New System.Drawing.Size(139, 23)
+        Me.addProcess.TabIndex = 1
+        Me.addProcess.Text = "Add Selected Process"
+        Me.addProcess.UseVisualStyleBackColor = True
+        '
+        'processSize
+        '
+        Me.processSize.Location = New System.Drawing.Point(12, 230)
+        Me.processSize.Name = "processSize"
+        Me.processSize.Size = New System.Drawing.Size(281, 20)
+        Me.processSize.TabIndex = 2
+        '
+        'selectedProcess
+        '
+        Me.selectedProcess.FormattingEnabled = True
+        Me.selectedProcess.Items.AddRange(New Object() {"P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8", "P9", "P10"})
+        Me.selectedProcess.Location = New System.Drawing.Point(36, 135)
+        Me.selectedProcess.Name = "selectedProcess"
+        Me.selectedProcess.Size = New System.Drawing.Size(100, 21)
+        Me.selectedProcess.TabIndex = 3
+        Me.selectedProcess.Text = "P1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(43, 119)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(81, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Select Process:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(33, 214)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(142, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Enter size (in KB) of process:"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(340, 303)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(302, 134)
+        Me.ListBox1.TabIndex = 6
+        '
+        'removeProcess
+        '
+        Me.removeProcess.Location = New System.Drawing.Point(65, 285)
+        Me.removeProcess.Name = "removeProcess"
+        Me.removeProcess.Size = New System.Drawing.Size(164, 23)
+        Me.removeProcess.TabIndex = 7
+        Me.removeProcess.Text = "Remove Selected Process"
+        Me.removeProcess.UseVisualStyleBackColor = True
+        '
+        'selectedAlgorithm
+        '
+        Me.selectedAlgorithm.FormattingEnabled = True
+        Me.selectedAlgorithm.Items.AddRange(New Object() {"First Fit"})
+        Me.selectedAlgorithm.Location = New System.Drawing.Point(174, 135)
+        Me.selectedAlgorithm.Name = "selectedAlgorithm"
+        Me.selectedAlgorithm.Size = New System.Drawing.Size(100, 21)
+        Me.selectedAlgorithm.TabIndex = 8
+        Me.selectedAlgorithm.Text = "First Fit"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(188, 119)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(86, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Select Algorithm:"
+        '
+        'compactMemory
+        '
+        Me.compactMemory.Location = New System.Drawing.Point(433, 440)
+        Me.compactMemory.Name = "compactMemory"
+        Me.compactMemory.Size = New System.Drawing.Size(100, 26)
+        Me.compactMemory.TabIndex = 10
+        Me.compactMemory.Text = "Compact Memory"
+        Me.compactMemory.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(62, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(217, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Enter max. size of memory (default 4096 KB):"
+        '
+        'maxSize
+        '
+        Me.maxSize.Location = New System.Drawing.Point(12, 25)
+        Me.maxSize.Name = "maxSize"
+        Me.maxSize.Size = New System.Drawing.Size(281, 20)
+        Me.maxSize.TabIndex = 12
+        '
+        'changeSize
+        '
+        Me.changeSize.Location = New System.Drawing.Point(46, 51)
+        Me.changeSize.Name = "changeSize"
+        Me.changeSize.Size = New System.Drawing.Size(219, 23)
+        Me.changeSize.TabIndex = 13
+        Me.changeSize.Text = "Change max. size of memory"
+        Me.changeSize.UseVisualStyleBackColor = True
+        '
+        'exitButton
+        '
+        Me.exitButton.Location = New System.Drawing.Point(567, 501)
+        Me.exitButton.Name = "exitButton"
+        Me.exitButton.Size = New System.Drawing.Size(75, 23)
+        Me.exitButton.TabIndex = 14
+        Me.exitButton.Text = "Exit"
+        Me.exitButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(654, 545)
+        Me.Controls.Add(Me.exitButton)
+        Me.Controls.Add(Me.changeSize)
+        Me.Controls.Add(Me.maxSize)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.compactMemory)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.selectedAlgorithm)
+        Me.Controls.Add(Me.removeProcess)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.selectedProcess)
+        Me.Controls.Add(Me.processSize)
+        Me.Controls.Add(Me.addProcess)
+        Me.Name = "Form1"
+        Me.Text = "Memory Manager"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents addProcess As Button
+    Friend WithEvents processSize As TextBox
+    Friend WithEvents selectedProcess As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents removeProcess As Button
+    Friend WithEvents selectedAlgorithm As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents compactMemory As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents maxSize As TextBox
+    Friend WithEvents changeSize As Button
+    Friend WithEvents exitButton As Button
+End Class
---
+++ Memory Manager/Form1.vb
@@ -0,0 +1,260 @@
+﻿'Daniel Rossano
+Public Class Form1
+    'Maximum amount of memory; can be changed, but will be 4096 KB by default
+    Dim memSize As Integer = 4096
+    'Variable to track the amount of memory currently available, which of course by default is all of it
+    Dim availMem As Integer = memSize
+    'Integer array to keep track of which processes are currently in memory, as well as where they are
+    Dim memory(9) As Integer
+    'Integer array to keep track of any holes left behind by previous processes
+    Dim holes As Integer() = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    'Since allocation algorithms decide where the processes go, we will use another array to track which processes are already accounted for
+    Dim processes(9) As Integer
+
+    Private Sub addProcess_Click(sender As Object, e As EventArgs) Handles addProcess.Click
+
+        Dim addedProcess As Integer
+        'first check that the user entered an acceptable data type for memory: one that is not a string
+        On Error GoTo error_handler
+        addedProcess = CInt(processSize.Text)
+
+        'next we will check if the user is trying to edit the memory available for a process already in memory
+        If processes(selectedProcess.SelectedIndex) <> 0 Then
+            processSize.Text = "Process already in memory"
+            Exit Sub
+        End If
+
+        'now check that the value is small enough to fit in available memory, while still being a positive integer
+        If addedProcess > availMem Then
+            processSize.Text = "Process too big to fit in memory"
+            Exit Sub
+        End If
+        If addedProcess <= 0 Then
+            GoTo error_handler
+        End If
+
+        'next check that adding this process won't go over the size limit of the available memory
+
+        Dim totalSpace As Integer = 0
+        For index As Integer = 0 To 9
+            'first find all of the space taken up by the existing processes
+            totalSpace = totalSpace + memory(index)
+            'while we are at it, we will also check if the process' size is unique
+            'this is a requirement for my project as it is difficult to remove a process from memory if there are other processes of the same size
+            If processes(index) = addedProcess Then
+                processSize.Text = "Process sizes must be unique; there is already a process of " & addedProcess & " KB in memory"
+                Exit Sub
+            End If
+        Next
+        'Now add in the space taken up by the new process and check if the sum is too big to fit in memory
+        totalSpace = totalSpace + addedProcess
+        If totalSpace > memSize Then
+            processSize.Text = "Not enough space in memory"
+            memory(selectedProcess.SelectedIndex) = 0
+        End If
+
+        'now that the added process has been "screened" we can run the allocation algorithms.
+        If selectedAlgorithm.SelectedItem = "First Fit" Then
+            For index As Integer = 0 To 9
+                'if we have found a hole big enough to fit the process
+                If (holes(index) >= addedProcess) Then
+                    'if there is no process at the current index,
+                    If memory(index) = 0 Then
+                        memory(index) = addedProcess
+                        holes(index) = holes(index) - addedProcess
+                        Exit For
+                        'if there IS a process at the current index, then we need to "squeeze it in"
+                    Else
+                        'start by "shifting over" the nonzero processes
+                        For index1 As Integer = 0 To 9
+                            If memory(index1) = 0 Then
+                                For index2 As Integer = 0 To 9
+                                    If memory(index2) <> 0 Then
+                                        memory(index1) = memory(index2)
+                                        memory(index2) = 0
+                                        Exit For
+                                    End If
+                                Next
+                            End If
+                        Next
+                        'now add in the process
+                        memory(index) = addedProcess
+                        holes(index) = holes(index) - addedProcess
+                        Exit For
+                    End If
+                    'if there is already space for the process, then add it in
+                ElseIf (memory(index) = 0) Then
+                    memory(index) = addedProcess
+                    Exit For
+                End If
+            Next
+        End If
+        'now, remove the space the process takes up from available memory
+        availMem = availMem - addedProcess
+        ListBox1.Items.Add(addedProcess)
+
+        'the process is in memory, but possibly in a different index than the one the user has selected
+        'to prevent the same process from being added multiple times, we make use of an array which accounts for the selected processes
+        processes(selectedProcess.SelectedIndex) = addedProcess
+
+
+        ListBox1.Items.Clear()
+        ListBox1.Items.Add("Current contents of memory are:")
+        For index1 As Integer = 0 To 9
+            If memory(index1) <> 0 Then
+                For index2 As Integer = 0 To 9
+                    If processes(index2) = memory(index1) Then
+                        ListBox1.Items.Add("Process " & index2 + 1 & " with " & memory(index1) & " KB")
+                        Exit For
+                    End If
+                Next
+            End If
+            If holes(index1) <> 0 Then
+                ListBox1.Items.Add("Hole of " & holes(index1) & " KB")
+            End If
+        Next
+
+        Exit Sub
+
+error_handler:
+        processSize.Text = "Only positive integers are accepted"
+        memory(selectedProcess.SelectedIndex) = 0
+    End Sub
+
+    Private Sub removeProcess_Click(sender As Object, e As EventArgs) Handles removeProcess.Click
+        If processes(selectedProcess.SelectedIndex) <> 0 Then
+            'first find the process' location in memory 
+            'since each process' size is unique, we just search the memory array until we find a process whose size matches that of the selected process
+            For index As Integer = 0 To 9
+                If memory(index) = processes(selectedProcess.SelectedIndex) Then
+                    holes(index) = holes(index) + memory(index)
+                    memory(index) = 0
+                    processes(selectedProcess.SelectedIndex) = 0
+
+                    Exit For
+                End If
+            Next
+
+        Else
+            Exit Sub
+        End If
+
+
+        'calculate how much memory is now available
+        availMem = memSize
+        For index As Integer = 0 To 9
+            availMem = availMem - memory(index)
+        Next
+
+
+
+
+        ListBox1.Items.Clear()
+        ListBox1.Items.Add("Current contents of memory are:")
+        For index1 As Integer = 0 To 9
+            If memory(index1) <> 0 Then
+                For index2 As Integer = 0 To 9
+                    If processes(index2) = memory(index1) Then
+                        ListBox1.Items.Add("Process " & index2 + 1 & " with " & memory(index1) & " KB")
+                        Exit For
+                    End If
+                Next
+            End If
+            If holes(index1) <> 0 Then
+                ListBox1.Items.Add("Hole of " & holes(index1) & " KB")
+            End If
+        Next
+    End Sub
+
+    Private Sub changeSize_Click(sender As Object, e As EventArgs) Handles changeSize.Click
+        If maxSize.Text.Trim.Equals("") Then
+            memSize = 4096
+
+            availMem = memSize
+            'calculate how much memory is available
+            For index As Integer = 0 To 9
+                availMem = availMem - memory(index)
+            Next
+
+        End If
+        'If the user enters a string instead of a number, go to the error handler
+        On Error GoTo error_handler
+        memSize = CInt(maxSize.Text.Trim)
+        maxSize.Text = memSize
+        If (memSize <= 0) Then
+            maxSize.Text = "Only positive integers are accepted"
+            memSize = 4096
+
+            availMem = memSize
+            'calculate how much memory is available
+            For index As Integer = 0 To 9
+                availMem = availMem - memory(index)
+            Next
+
+        End If
+
+
+        'check to see if the new memory size can fit the previous processes
+        For index As Integer = 0 To 9
+            If memory(index) > memSize Then
+                memSize = memSize + memory(index)
+                maxSize.Text = "Specified size has been changed to fit all existing processes"
+            End If
+        Next
+
+        'calculate how much memory is available
+        availMem = memSize
+        For index As Integer = 0 To 9
+            availMem = availMem - memory(index)
+        Next
+
+
+        ListBox1.Items.Add("Memory size is now " & memSize)
+        Exit Sub
+error_handler:
+        maxSize.Text = "Only positive integers are accepted"
+        memSize = 4096
+
+        availMem = memSize
+        'calculate how much memory is available
+        For index As Integer = 0 To 9
+            availMem = availMem - memory(index)
+        Next
+    End Sub
+
+    Private Sub exitButton_Click(sender As Object, e As EventArgs) Handles exitButton.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub compactMemory_Click(sender As Object, e As EventArgs) Handles compactMemory.Click
+        For index1 As Integer = 0 To 9
+            'fill up any "gaps" in memory
+            If memory(index1) = 0 Then
+                For index2 As Integer = 0 To 9
+                    If memory(index2) <> 0 Then
+                        memory(index1) = memory(index2)
+                        memory(index2) = 0
+                        Exit For
+                    End If
+                Next
+            End If
+            'all of the valid processes are now contiguous, so we no longer have any holes
+            holes(index1) = 0
+        Next
+        ListBox1.Items.Clear()
+        ListBox1.Items.Add("Current contents of memory are:")
+        For index1 As Integer = 0 To 9
+            If memory(index1) <> 0 Then
+                For index2 As Integer = 0 To 9
+                    If processes(index2) = memory(index1) Then
+                        ListBox1.Items.Add("Process " & index2 + 1 & " with " & memory(index1) & " KB")
+                        Exit For
+                    End If
+                Next
+            End If
+            If holes(index1) <> 0 Then
+                ListBox1.Items.Add("Hole of " & holes(index1) & " KB")
+            End If
+        Next
+    End Sub
+End Class
